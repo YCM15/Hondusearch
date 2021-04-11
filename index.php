26a305e2ac74a4e9728821491b81b630f9275976
AnibalDiaz <anibalmauriciod@gmail.com>
2021-04-11 17:56:51 -0600

    a;adiendo a heroku desde master

--- index.php
+++ index.php
@@ -1,336 +1,231 @@
-<<<<<<< HEAD:index.php
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HONDU SEARCH</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Stint+Ultra+Condensed" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-    <link rel="stylesheet" href="css/estilos.css">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HONDU SEARCH</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Stint+Ultra+Condensed" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/estilos.css">
 
 </head>
+
 <body>
-    <!--logo -->
-    <header  class = "logo" >
-      <h1 >
-        <img  src = "img/logo.png" alt = " Logotipo de Fazt "  >
-      </h1 >
-      
-    </header>
-    
-    <p><hr width=800></p>
+  <!--logo -->
+  <header class="logo container">
+    <h1>
+      <img src="img/logo.png" class="img img-fluid" alt=" Logotipo de Fazt ">
+    </h1>
+  </header>
+
+  <p>
     <!-- NAVEGACION -->
-    <div  class = " navbar " >
-     
-      <a  href = "./" > Inicio </a>
-      <a  href = "#" > Quiénes Somos </a>
-      <a  href = "#" > Servicios </a>
-      <a  href = "#" > Acerca de </a>
-      <a  href = "Login/login.php" > Ingresar </a>
-      <a  href = "Login/registro.php" > Registro </a>
+  <div class=" navbar nav ">
+
+    <a href="./"> Inicio </a>
+    <a href="#"> Quiénes Somos </a>
+    <a href="#"> Servicios </a>
+    <a href="empleos/index.php"> Empleos</a>
+    <a href="Login/login.php"> Ingresar </a>
+    <a href="Login/registro.php"> Registro </a>
+  </div>
+  <!-- BIENVENIDA -->
+  <div class="container mt-5 bienvenida">
+    <div class="row mt-5 mb-5">
+      <div class="col-md-6 mt-5">
+        <table width="380" cellspacing="2" cellpadding="3" borde="0">
+          <tr>
+            <td>
+              <h2 class="title">La manera correcta de hacer negocios </h2>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div class="col-md-6 text-center">
+        <img src="./img/team.png">
+      </div>
+    </div>
+  </div>
+
+
+  <!-- quienes somos -->
+  <div class="row">
+    <div class="col-md-12">
+      <div class="quienes">
+        <h2 class="title">Quienes Somos</h2>
+        <p>Desde sus comienzos en el año2000, HONDU SEARCH es conocido por su compromiso auténtico
+          con la satisfacción del cliente. Este estándar de excelencia nos ha permitido
+          crecer hasta llegar a donde estamos ahora.</p>
+
+        <p> Nuestros clientes son lo principal para nosotros y para ellos tenemos servicios y
+          productos excepcionales. Contáctanos para saber más acerca de lo que ofrecemos.</p>
+      </div>
     </div>
 
-       <!-- BIENVENIDA -->
-    <div class="bienvenida">
-      
-         
 
-  
-          <table  width="380" cellspacing="2" cellpadding="3" borde="0">
-            <tr>
-               <td>
-                <h2 class="title" >La manera correcta de hacer negocios </h2>
-               </td>
-            </tr>
-            
-            </table>
-              
-    </div>
-    
-
-   <!-- quienes somos -->
-   <div class="quienes">
-    <h2 class="title" >Quienes Somos</h2>
-    <p>Desde sus comienzos en el año2000, HONDU SEARCH es conocido por su compromiso auténtico
-    con la satisfacción del cliente. Este estándar de excelencia nos ha permitido 
-        crecer hasta llegar a donde estamos ahora.</p>
-
-    <p> Nuestros clientes son lo principal para nosotros y para ellos tenemos servicios y 
-        productos excepcionales. Contáctanos para saber más acerca de lo que ofrecemos.</p>
-   </div>
-
-  
+  </div>
 
 
-   <!-- servicios -->
-
-  <div class="servicios">
-    <h2 class="title">Nuestros Servicios</h2>
-    <p>superamos tus expectativas</p>
 
 
-  
-            <ul class="list-services">
-                <li>
-                  <figure>
-                    <img src="img/servicio.png" alt="" width="100" height="100" >
-                    <figcaption>Desarrollador web</figcaption>
-                  </figure>
-                </li>
-          
-      
-        
-            <ul class="list-services">
-                <li>
-                  <figure>
-                    <img src="img/Background (4).png" alt="" width="90" height="100" >
-                    <figcaption>Desarrollador desktop</figcaption>
-                  </figure>
-                </li>
-        
-          <ul class="list-services">
+
+  <!-- servicios -->
+  <div>
+
+    <div class="row">
+      <div class="servicios">
+        <h2 class="title">Nuestros Servicios</h2>
+        <p>superamos tus expectativas</p>
+
+
+        <ul class="list-services col-md-12">
+          <li>
+            <figure>
+              <img src="img/servicio.png" alt="" width="100" height="100">
+              <figcaption>Desarrollador web</figcaption>
+            </figure>
+          </li>
+
+
+
+          <ul class="list-services col-md-4">
             <li>
               <figure>
-                <img src="img/movil.png" alt="" width="90" height="100" >
-                <figcaption>Desarrollador movil o apps</figcaption>
+                <img src="img/Background (4).png" alt="" width="90" height="100">
+                <figcaption>Desarrollador desktop</figcaption>
               </figure>
             </li>
-    
+
+            <ul class="list-services col-md-4">
+              <li>
+                <figure>
+                  <img src="img/movil.png" alt="" width="90" height="100">
+                  <figcaption>Desarrollador movil o apps</figcaption>
+                </figure>
+              </li>
+
+      </div>
     </div>
-
-
-            <!-- entrega -->
-    <div class="entrega">
-
-        <article class="contain">
-            <h2 class="title">Entrega programada</h2>
-            <table  width="380" cellspacing="2" cellpadding="3" borde="0">
-              <tr>
-                 <td>
-                  <p>Estamos aquí para servirte</p>
-                  <p>Queremos que todos nuestros clientes puedan experimentar el nivel de profesionalismo de 
-                      nuestro equipo al colaborar con HONDU SEARCH. La razón de ser de nuestros servicios,
-                      especialmente de este, es mejorar tu vida. 
-                  <p>Tenemos los mejores productos y nuestra atención al cliente es incomparable.</p>
-                 </td>
-              </tr>
-              
-              </table>
-          
-        </article>  
-    </div>
-
- 
+  </div>
 
 
 
+  <!-- entrega -->
+  <div class="entrega row ">
+      <div class="col-md-6">
+        <img class="img img-fluid" src="./img/1.png">
+      </div>
+      <div class="col-md-6">
+        <h2 class="title">Entrega programada</h2>
+        <p class="text-white">Estamos aquí para servirte</p>
+        <p class="text-white">Queremos que todos nuestros clientes puedan experimentar el nivel de profesionalismo de
+          nuestro equipo al colaborar con HONDU SEARCH. La razón de ser de nuestros servicios,
+          especialmente de este, es mejorar tu vida.
+        <p class="text-white">Tenemos los mejores productos y nuestra atención al cliente es incomparable.</p>
+      </div>
+  </div>
 
-  <!-- seguisr -->
-  <div class="seguir">
-  <h1 class="title">Seguir</h1>
 
-  
-  <ul class="list-seguir">
-    <li>
-      <figure>
-            <img  src = "img/facebook.png" alt = " Logotipo de Fazt " width="30" height="30" >
-        </figure>
-    </li>
 
-    <li>
-        <figure>
-          <img  src = "img/twitter.png" alt = " Logotipo de Fazt " width="30" height="30" >
-        </figure>
-    </li>
-      
+  </div>
 
-     <li>
-       <figure>
-          <img  src = "img/instagram.png" alt = " Logotipo de Fazt " width="30" height="30" >
-        </figure>
-    </li>
-     
-    <li>
-       <figure>
-         <img  src = "img/linkedin.png" alt = " Logotipo de Fazt " width="30" height="30">
-        </figure>
-    </li>
+</footer>
+<footer class="text-center text-white" style="background-color: #caced1;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Images -->
+    <section class="ml-5">
+      <div class="row ml-5">
+      <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+      </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="img/facebook.png""
+              class="w-50"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="img/instagram.png"
+              class="w-50"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="img/twitter.png"
+              class="w-50"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            <img
+              src="img/linkedin.png"
+              class="w-50"
+            />
+            <a href="#!">
+              <div
+                class="mask"
+                style="background-color: rgba(251, 251, 251, 0.2);"
+              ></div>
+            </a>
+          </div>
+        </div>
+    </section>
+    <!-- Section: Images -->
+  </div>
+  <!-- Grid container -->
 
-    
-</div>
-       
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2021 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">Hondusearch</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 
 
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HONDU SEARCH</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Stint+Ultra+Condensed" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
-    <link rel="stylesheet" href="css/estilos.css">
-
-</head>
-<body>
-    <!--logo -->
-    <header  class = "logo" >
-      <h1 >
-        <img  src = "img/logo.png" alt = " Logotipo de Fazt "  >
-      </h1 >
-      
-    </header>
-    
-    <p><hr width=800></p>
-    <!-- NAVEGACION -->
-    <div  class = " navbar " >
-     
-      <a  href = "./" > Inicio </a>
-      <a  href = "#" > Quiénes Somos </a>
-      <a  href = "#" > Servicios </a>
-      <a  href = "#" > Acerca de </a>
-      <a  href = "Login/login.php" > Ingresar </a>
-      <a  href = "Login/registro.php" > Registro </a>
-    </div>
-
-       <!-- BIENVENIDA -->
-    <div class="bienvenida">
-      
-         
-
-  
-          <table  width="380" cellspacing="2" cellpadding="3" borde="0">
-            <tr>
-               <td>
-                <h2 class="title" >La manera correcta de hacer negocios </h2>
-               </td>
-            </tr>
-            
-            </table>
-              
-    </div>
-    
-
-   <!-- quienes somos -->
-   <div class="quienes">
-    <h2 class="title" >Quienes Somos</h2>
-    <p>Desde sus comienzos en el año2000, HONDU SEARCH es conocido por su compromiso auténtico
-    con la satisfacción del cliente. Este estándar de excelencia nos ha permitido 
-        crecer hasta llegar a donde estamos ahora.</p>
-
-    <p> Nuestros clientes son lo principal para nosotros y para ellos tenemos servicios y 
-        productos excepcionales. Contáctanos para saber más acerca de lo que ofrecemos.</p>
-   </div>
-
-  
-
-
-   <!-- servicios -->
-
-  <div class="servicios">
-    <h2 class="title">Nuestros Servicios</h2>
-    <p>superamos tus expectativas</p>
-
-
-  
-            <ul class="list-services">
-                <li>
-                  <figure>
-                    <img src="img/servicio.png" alt="" width="100" height="100" >
-                    <figcaption>Desarrollador web</figcaption>
-                  </figure>
-                </li>
-          
-      
-        
-            <ul class="list-services">
-                <li>
-                  <figure>
-                    <img src="img/Background (4).png" alt="" width="90" height="100" >
-                    <figcaption>Desarrollador desktop</figcaption>
-                  </figure>
-                </li>
-        
-          <ul class="list-services">
-            <li>
-              <figure>
-                <img src="img/movil.png" alt="" width="90" height="100" >
-                <figcaption>Desarrollador movil o apps</figcaption>
-              </figure>
-            </li>
-    
-    </div>
-
-
-            <!-- entrega -->
-    <div class="entrega">
-
-        <article class="contain">
-            <h2 class="title">Entrega programada</h2>
-            <table  width="380" cellspacing="2" cellpadding="3" borde="0">
-              <tr>
-                 <td>
-                  <p>Estamos aquí para servirte</p>
-                  <p>Queremos que todos nuestros clientes puedan experimentar el nivel de profesionalismo de 
-                      nuestro equipo al colaborar con HONDU SEARCH. La razón de ser de nuestros servicios,
-                      especialmente de este, es mejorar tu vida. 
-                  <p>Tenemos los mejores productos y nuestra atención al cliente es incomparable.</p>
-                 </td>
-              </tr>
-              
-              </table>
-          
-        </article>  
-    </div>
-
- 
-
-
-
-
-  <!-- seguir -->
-  <div class="seguir">
-  <h1 class="title">Seguir</h1>
-
-  
-  <ul class="list-seguir">
-    <li>
-      <figure>
-            <img  src = "img/facebook.png" alt = " Logotipo de Fazt " width="30" height="30" >
-        </figure>
-    </li>
-
-    <li>
-        <figure>
-          <img  src = "img/twitter.png" alt = " Logotipo de Fazt " width="30" height="30" >
-        </figure>
-    </li>
-      
-
-     <li>
-       <figure>
-          <img  src = "img/instagram.png" alt = " Logotipo de Fazt " width="30" height="30" >
-        </figure>
-    </li>
-     
-    <li>
-       <figure>
-         <img  src = "img/linkedin.png" alt = " Logotipo de Fazt " width="30" height="30">
-        </figure>
-    </li>
-
-    
-</div>
-       
-
-
-</body>
->>>>>>> b4381ac0509a0993506169d2f8545f120976b356:landing.html
 </html>
--- js/validar.js
+++ js/validar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function codeTel(){
         prefijo = $("#pais option:selected").val();
         $("#telefono").val("+"+prefijo+" - ");
@@ -22,29 +21,4 @@ function serializar(){
         object[llave.name]=llave.value;
     });
     return object;
-=======
-function codeTel(){
-        prefijo = $("#pais option:selected").val();
-        $("#telefono").val("+"+prefijo+" - ");
-    }
-
-function validar(){
-    $("#form").addClass("was-validated");
-    var inputs = document.getElementsByClassName("form-control");
-    for(i=0;i<inputs.length;i++){
-      if(inputs[i].checkValidity()===false){
-        return false;
-      }
-    }
-    return true;
-}
-
-function serializar(){
-    var array = $("form").serializeArray();
-    var object = {};
-    $.each(array, function(indice, llave){
-        object[llave.name]=llave.value;
-    });
-    return object;
->>>>>>> b4381ac0509a0993506169d2f8545f120976b356
 }
